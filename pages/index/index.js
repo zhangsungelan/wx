20b8adb4505a2dfb2a3e3855f9051a9bca4b7d5b
zhangsun <tz483835201@163.com>
2019-09-11 21:30:35 +0800

    '注册相关'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,31 @@
 //index.js
-//获取应用实例
-const app = getApp()
+//获取app实例
+const app=getApp();
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    data:{
+        name:"zhangsun"
+    },
+    //监听页面加载
+    onLoad(){
+
+    },
+    //页面显示
+    onShow(){
+
+    },
+    //监听页面初次渲染完成
+    onReady(){
+
+    },
+    //监听下拉刷新
+    onPullDownRefresh(){
+        console.log("已经没有了")
+    },
+    onReachBottom(){
+        console.log("已经没有了")
+    },
+    gewxtUserInfo(event){
+        console.log(event.detail.userInfo)
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
